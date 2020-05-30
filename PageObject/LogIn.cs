09d0107c83b3be51a2cc1b1d54a5ccb08502197c
Biljana Dutina <biljana.dutina@yahoo.com>
2020-05-30 22:27:41 +0200

    Add project files.

---
+++ PageObject/LogIn.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace ConsoleApp_LogIn.PageObject
+{
+    class LogIn
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public LogIn (IWebDriver driver)
+        {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+        public IWebElement Username
+        {
+            get
+            {
+                IWebElement element;
+                try
+                {
+                    element = this.driver.FindElement(By.Name("username"));
+                }
+                catch (Exception)
+                {
+                    element = null;
+                }
+                return element;
+            }
+        }
+             public IWebElement Password
+        {
+            get
+            {
+                IWebElement element;
+                try
+                {
+                    element = this.driver.FindElement(By.Name("password"));
+                }
+                catch (Exception)
+                {
+                    element = null;
+                }
+                return element;
+            }
+        }
+        public IWebElement RegButton
+        {
+            get
+            {
+                IWebElement element;
+                try
+                {
+                    element = this.driver.FindElement(By.Name("login"));
+                }
+                catch (Exception)
+                {
+                    element = null;
+                }
+                return element;
+            }
+        }
+    }
+    
+}
---
+++ Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
+using System.Collections.ObjectModel;
+using System.Threading;
+using ConsoleApp_LogIn.PageObject;
+
+namespace ConsoleApp_LogIn
+{
+    class Test
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+        [Test]
+        public void TestLogIn()
+        {
+            HomePage naslovna = new HomePage(driver);
+            LogIn registracija;
+            naslovna.GoToPage();
+            registracija = naslovna.SignButton();
+            registracija.Username.SendKeys("ppetar");
+            registracija.Password.SendKeys("pppetar");
+            registracija.RegButton.Click();
+        }
+    }
+}
